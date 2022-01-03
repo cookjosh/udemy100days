4a4dc8f0d154a1d197aeecaf9511f2b47f536707
Josh Cook <joshua.c.cook@protonmail.com>
2022-01-02 21:11:29 -0800

    First commit. Day 1 of 100 Days of Code - Udemy. Random Band Name Generator!

---
+++ day1/README.md
@@ -0,0 +1,3 @@
+# Project: Band Name Generator
+
+My submission for the Day 1 project Band Name Generator for Udemy's 100 Days of Code (Python bootcamp).
---
+++ day1/bandnamegenerator.py
@@ -0,0 +1,13 @@
+""" Band Name Generator
+1. Create a greeting for your program
+2. Ask the user for the city that they grew up in.
+3. Ask the user for the name of a pet.
+4. Combine the name of their city and pet and show them their band name.
+5. Make sure the input cursor shows on a new line
+"""
+
+# 1. Create a greeting for the program:
+print("Hello, and welcome to the Random Band Name Generator!")
+city = input("Please input the city you grew up in: \n")
+pet = input("And how about the name of a pet: \n")
+print(f"Your band name is {city} {pet}!")
