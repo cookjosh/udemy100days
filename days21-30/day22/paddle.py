bf7714b1b82a77ba9933eebbdcbebd56915236e3
Josh Cook <joshua.c.cook@protonmail.com>
2022-03-03 20:31:47 -0800

    Added paddle class and refactored main.py

---
+++ days21-30/day22/paddle.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, xcoord):
+        super().__init__()
+        self.penup()
+        self.setx(xcoord)
+        self.shape("square")
+        self.turtlesize(stretch_wid=1, stretch_len=4)
+        self.fillcolor("white")
+        self.setheading(90)
+
+    def move_up(self):
+        self.forward(50)
+
+    def move_down(self):
+        self.backward(50)
