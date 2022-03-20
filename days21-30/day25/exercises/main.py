53ed9a774a9c06412f2cec333f3e3a70d7035f0c
Josh Cook <joshua.c.cook@protonmail.com>
2022-03-20 13:58:00 -0700

    Day 25 second commit. More Pandas

--- days21-30/day25/exercises/main.py
+++ days21-30/day25/exercises/main.py
@@ -20,5 +20,43 @@ with open("weather_data.csv") as file:
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+"""
+#print(data)
+#print(data["temp"])
+
+data_dict = data.to_dict()
+
+temp_list = data["temp"].to_list()
+
+# Get average temperature
+average_temp = sum(temp_list) / len(temp_list)
+print(average_temp)
+# OR using Pandas
+print(data["temp"].mean())
+
+# Print largest number in temp series
+print(data["temp"].nlargest(1))
+# OR
+print(data["temp"].max()) # Cleaner
+
+# Selecting columns - two methods
 print(data["temp"])
+print(data.temp)
+
+
+# Getting data from rows
+print(data[data.temp == data.temp.max()])
+
+# Convert a particular day's temperature to Farenheit
+monday_data = data[data.day == "Monday"]
+monday_farenheit = (int(monday_data.temp) * 1.8) + 32
+print(monday_farenheit)
+"""
+
+# Create DataFrame from scratch - example data
+data_dict = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
