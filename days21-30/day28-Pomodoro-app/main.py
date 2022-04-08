d185c35160726163c20874fc62fde85f35a505a4
Josh Cook <joshua.c.cook@protonmail.com>
2022-04-07 21:43:17 -0700

    Day 28 project - Pomodoro app

--- days21-30/day28-Pomodoro-app/main.py
+++ days21-30/day28-Pomodoro-app/main.py
@@ -2,6 +2,7 @@
 # Using tkinter, create a GUI timer for the Pomodoro method
 # Starting code (Constants section) and tomato img provided by Udemy course
 
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -15,10 +16,21 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    canvas.create_text(253, 175, text="00:00", fill="white", font=(FONT_NAME, 14, "bold"))
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    countdown(1500)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(time_text, text=f"{minutes}:{seconds}")
+    if count == 1500:
+        canvas.itemconfig(time_text, text=f"{minutes}:{seconds}0")
+        window.after(1000, countdown, count - 1)
+    elif count > 0:
+        window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,8 +40,9 @@ window.config(padx=50, pady=100, bg=GREEN)
 canvas = Canvas(width=500, height=300, bg=GREEN, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image(250, 150, image=tomato_image)
-canvas.create_text(253, 175, text="00:00", fill="white", font=(FONT_NAME, 14, "bold"))
+time_text = canvas.create_text(253, 175, text="00:00", fill="white", font=(FONT_NAME, 14, "bold"))
 canvas.grid(column=1, row=1)
+
 
 # Labels
 canvas_label = Label(text="Pomodoro Timer", font=(FONT_NAME, 20, "bold"), fg="white", bg=GREEN)
@@ -40,15 +53,10 @@ checkmark_label.grid(column=1, row=2)
 
 
 # Button things
-def click_start():
-    pass
-def click_reset():
-    pass
-
-start_button = Button(text="Start", command=click_start)
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=1)
 
-reset_button = Button(text="Reset", command=click_reset)
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.grid(column=3, row=1)
 
 
