bd7d444fcd3b1b916ab576d2ae7ec45bcf978b70
Josh Cook <joshua.c.cook@protonmail.com>
2022-04-10 21:58:45 -0700

    Project final commit. Added password generator functionality.

--- days21-30/day29-gui-password-mgr/main.py
+++ days21-30/day29-gui-password-mgr/main.py
@@ -8,12 +8,48 @@
 from email.mime.message import MIMEMessage
 from tkinter import *
 from tkinter.messagebox import askokcancel, showinfo
+import random
+
 
 GREEN = "#9bdeac"
 FONT_NAME = "Arial"
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+# This is taken from the course and not my own generator project for simplicity
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def random_generator():
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+    return password
+    
+
+def password_generator():
+    random_password = random_generator()
+    password_entry.insert(0, random_password)
+
+
+
 
 
 
@@ -68,7 +104,7 @@ password_label = Label(text="Password:", font=(FONT_NAME, 10), bg=GREEN)
 password_label.grid(column=0, row=3)
 password_entry = Entry(width=28)
 password_entry.grid(column=1, row=3)
-password_button = Button(text="Generate")
+password_button = Button(text="Generate", command=password_generator)
 password_button.grid(column=2, row=3)
 
 
