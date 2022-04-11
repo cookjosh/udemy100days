80b342708881cb7586ca95d59ec3bec2ad6d09e4
Josh Cook <joshua.c.cook@protonmail.com>
2022-04-10 22:00:25 -0700

    Added test entries to txt file.

--- days21-30/day29-gui-password-mgr/main.py
+++ days21-30/day29-gui-password-mgr/main.py
@@ -17,7 +17,8 @@ FONT_NAME = "Arial"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 # Password Generator Project
-# This is taken from the course and not my own generator project for simplicity
+# random_generator func is from the course and not my own generator project for simplicity
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -47,10 +48,6 @@ def random_generator():
 def password_generator():
     random_password = random_generator()
     password_entry.insert(0, random_password)
-
-
-
-
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
