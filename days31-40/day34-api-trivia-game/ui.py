9acf5171412caa526380db7ab90669e140f14d95
Josh Cook <joshua.c.cook@protonmail.com>
2022-04-17 11:20:04 -0700

    Day 34 - final commit

--- days31-40/day34-api-trivia-game/ui.py
+++ days31-40/day34-api-trivia-game/ui.py
@@ -1,5 +1,6 @@
 from cgitb import text
 from tkinter import *
+from turtle import bgcolor
 from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
@@ -39,16 +40,31 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        self.score_label.config(text=f"score: {self.quiz.score}")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text=f"Congratulations! You've reached the end of the quiz. Your score is {self.quiz.score}!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def true_click(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        is_right = self.user_feedback(self.quiz.check_answer("True"))
 
     def false_click(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("False")
+        self.user_feedback(is_right)
+
+
+    def user_feedback(self, is_right):
+        if is_right == True:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
+
         
 
 
