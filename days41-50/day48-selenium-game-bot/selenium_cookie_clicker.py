7c3ef1499e5e1a0eff8235273a76fd3bc8ac90d5
Josh Cook <josh@Joshs-MacBook-Pro.local>
2022-07-11 21:30:05 -0500

    Moving to next day. Will return

--- days41-50/day48-selenium-game-bot/selenium_cookie_clicker.py
+++ days41-50/day48-selenium-game-bot/selenium_cookie_clicker.py
@@ -11,6 +11,40 @@ driver = webdriver.Chrome(executable_path = chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie_icon = driver.find_element(By.ID, "cookie")
 
+# Prices of upgrades
+# Refactor this to be a loop that pulls from list of upgrades
+# and inserts into find_element and creates price variable
+cursor_info = driver.find_element(By.XPATH, "//*[@id='buyCursor']/b")
+cursor_price = (cursor_info.text).split("- ", 1)[1]
+
+grandma_info = driver.find_element(By.XPATH, "//*[@id='buyGrandma']/b")
+grandma_price = (grandma_info.text).split("- ", 1)[1]
+
+factory_info = driver.find_element(By.XPATH, "//*[@id='buyFactory']/b")
+factory_price = (factory_info.text).split("- ", 1)[1]
+
+mine_info = driver.find_element(By.XPATH, "//*[@id='buyMine']/b")
+mine_price = (mine_info.text).split("- ", 1)[1]
+
+shipment_info = driver.find_element(By.XPATH, "//*[@id='buyShipment']/b")
+shipment_price = (shipment_info.text).split("- ", 1)[1]
+
+alchemy_info = driver.find_element(By.XPATH, "//*[@id='buyAlchemy lab']/b")
+alchemy_price = (alchemy_info.text).split("- ", 1)[1]
+
+portal_info = driver.find_element(By.XPATH, "//*[@id='buyPortal']/b")
+portal_price = (portal_info.text).split("- ", 1)[1]
+
+time_machine_info = driver.find_element(By.XPATH, "//*[@id='buyTime machine']/b")
+time_machine_price = (time_machine_info.text).split("- ", 1)[1]
+
+
+#def upgrades():
+
+
+
+"""
 game_on = True
 while game_on:
     cookie_icon.click()
+"""
