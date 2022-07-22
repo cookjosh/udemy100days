1506efc9dc64982e355e06b92501d710782a6c43
Josh Cook <jcook2303@gmail.com>
2022-07-21 22:13:04 -0700

    Day 57 exercises and final project

--- days51-60/day57-jinja-templating/exercises/server.py
+++ days51-60/day57-jinja-templating/exercises/server.py
@@ -19,6 +19,13 @@ def guess(user_name):
     user_gender = ((requests.get(url=genderize_url)).json())["gender"]
     return render_template("guess.html", name=user_name.capitalize(), age=user_age, gender=user_gender)
 
+@app.route("/blog")
+def get_blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
