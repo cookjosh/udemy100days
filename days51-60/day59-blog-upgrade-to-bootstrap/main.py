e575640f2f669e727cfd2d15685e85f6035d22cf
Josh Cook <jcook2303@gmail.com>
2022-09-19 22:31:22 -0700

    Last post for this course day. Some background images not showing up correctly.

--- days51-60/day59-blog-upgrade-to-bootstrap/main.py
+++ days51-60/day59-blog-upgrade-to-bootstrap/main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import requests
 app = Flask(__name__)
+
+response = requests.get('https://api.npoint.io/6aeb592b24ad9ad9d835')
+data = response.json()
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html', data=data)
 
 @app.route('/about')
 def about():
@@ -12,6 +16,17 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/post/<int:index>')
+def post(index):
+    requested_post = None
+    for post in data:
+        if post['id'] == index:
+            requested_post = post
+    return render_template('post.html', post=requested_post)
+
+
+
 
 if __name__ == '__main__':
    app.run(debug=True)
