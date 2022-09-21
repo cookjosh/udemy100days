10cd6c6adf8915b70aba6cd359d4b4b52a89a353
Josh Cook <jcook2303@gmail.com>
2022-09-20 21:30:55 -0700

    Adding second route that returns a different page when user submits form

--- days51-60/day60-post-flask-forms/main.py
+++ days51-60/day60-post-flask-forms/main.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, render_template
-import requests
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-    first_name = request.form.get('fname')
-    last_name = request.form.get('lname')
     return render_template('index.html')
+
+# Solution I came up with is different than course solution!
+@app.route('/login', methods = ['POST'])
+def receive_login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    data = {
+        'username': username,
+        'password': password,
+    }
+    return render_template('login.html', data = data)
+
+
 
 
 if __name__ == '__main__':
