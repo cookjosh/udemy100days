e2bb8951db3567ca07f6477e1cba4369255d5378
Josh Cook <jcook2303@gmail.com>
2022-10-03 21:33:19 -0700

    Completing day60 - flask_wtforms and flask-bootstrap

--- days61-70/day61-Flask-WTF/forms.py
+++ days61-70/day61-Flask-WTF/forms.py
@@ -1,6 +1,5 @@
 from tokenize import String
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 from werkzeug.utils import secure_filename
--- days61-70/day61-Flask-WTF/main.py
+++ days61-70/day61-Flask-WTF/main.py
@@ -1,21 +1,33 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 from forms import LoginForm
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
 
-app.secret_key = "password"
+    
 
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+    app.secret_key = "password"
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    form.validate_on_submit()
-    return render_template('login.html', form=form)
 
+    @app.route("/")
+    def home():
+        return render_template('index.html')
+
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            if form.email.data == "admin@email.com" and form.password.data == "12345678":
+                return render_template('success.html')
+            else:
+                return render_template('denied.html')
+        return render_template('login.html', form=form)
+
+    return app
 
 if __name__ == '__main__':
+    create_app()
     app.run(debug=True)
